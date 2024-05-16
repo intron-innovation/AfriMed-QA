4ced8d4194cc15bc7706ef5f20dd07b311611871
owos <owodunniabraham@gmail.com>
2024-05-16 13:37:28 +0000

    update script with fewshot options

--- scripts/run_phi3.sh
+++ scripts/run_phi3.sh
@@ -1,22 +1,22 @@
-#!/bin/sh
+#!/bin/bash
 
-pretrained_model_path=microsoft/Phi-3-mini-128k-instruct
+pretrained_model_path="microsoft/Phi-3-mini-128k-instruct"
 data_path="data/afri_med_qa_10k_v1_1_phase_1.csv"
-prompt_file_path="prompts/base_mcq.txt"
-python bin/main_predictions.py \
-    --data_path $data_path \
-    --prompt_file_path $prompt_file_path \
-    --pretrained_model_path ${pretrained_model_path} \
-    --q_type mcq
 
-python bin/main_predictions.py \
-    --data_path $data_path \
-    --prompt_file_path  $prompt_file_path\
-    --pretrained_model_path ${pretrained_model_path} \
-    --q_type saq
+question_types=("mcq" "saq" "consumer_queries")
+prompt_type=base
 
-python bin/main_predictions.py \
-    --data_path $data_path \
-    --prompt_file_path $prompt_file_path\
-    --pretrained_model_path ${pretrained_model_path} \
-    --q_type consumer_queries
+num_few_shot_values=(0 3 5)
+
+for q_type in "${question_types[@]}"; do
+    prompt_file_path="prompts/${prompt_type}_${q_type}.txt"
+    echo $prompt_file_path
+    for num_few_shot in "${num_few_shot_values[@]}"; do
+        python bin/main_predictions.py \
+            --data_path $data_path \
+            --prompt_file_path $prompt_file_path \
+            --pretrained_model_path $pretrained_model_path \
+            --q_type $q_type \
+            --num_few_shot $num_few_shot
+    done
+done
