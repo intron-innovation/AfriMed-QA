fc7779333f68817562aff5a628406df7e84524d1
Derguene <derguene.mbaye@baamtu.com>
2024-05-17 23:37:03 +0000

    edit models files

--- src/models/biomistral.py
+++ src/models/biomistral.py
@@ -7,33 +7,18 @@ class BioMistral(Model):
     def __init__(self, pretrained_model_path):
         super().__init__(pretrained_model_path)
 
-        self.tokenizer = AutoTokenizer.from_pretrained(
-            pretrained_model_path,
-            use_fast=False,
-            add_bos_token=False,
-            add_eos_token=False,
-            trust_remote_code=True,
-        )
-        self.model = AutoModel.from_pretrained(
-            pretrained_model_path,
-            device_map="auto",
-            use_safetensors=True,
-            torch_dtype=torch.bfloat16
-            if torch.cuda.is_bf16_supported()
-            else torch.float16,
-            trust_remote_code=True,
-        )
+        self.tokenizer = AutoTokenizer.from_pretrained(pretrained_model_path)
+        self.model = AutoModel.from_pretrained(pretrained_model_path)
         self.model = pipeline(
-            "text-generation", model=self.model, tokenizer=self.tokenizer,
+            "text-generation", model=self.model, tokenizer=self.tokenizer
         )
 
     def predict(self, prompt) -> str:
         prompt = [{"role": "user", "content": prompt}]
 
         output = self.model(prompt)
-        output = output[0]
+        output = output[0]["generated_text"]
         print(output)
-        output = output[0]["generated_text"].replace("<|end|>", "").strip(" ")
         return output
 
     def post_process(self, raw_text_output):
--- src/models/medalpaca.py
+++ src/models/medalpaca.py
@@ -6,34 +6,13 @@ from transformers import AutoTokenizer, AutoModel, pipeline
 class MedAlpaca(Model):
     def __init__(self, pretrained_model_path):
         super().__init__(pretrained_model_path)
-
-        self.tokenizer = AutoTokenizer.from_pretrained(
-            pretrained_model_path,
-            use_fast=False,
-            add_bos_token=False,
-            add_eos_token=False,
-            trust_remote_code=True,
-        )
-        self.model = AutoModel.from_pretrained(
-            pretrained_model_path,
-            device_map="auto",
-            use_safetensors=True,
-            torch_dtype=torch.bfloat16
-            if torch.cuda.is_bf16_supported()
-            else torch.float16,
-            trust_remote_code=True,
-        )
-        self.model = pipeline(
-            "text-generation", model=self.model, tokenizer=self.tokenizer,
-        )
+        self.model = pipeline("text-generation", model=pretrained_model_path, tokenizer=pretrained_model_path)
 
     def predict(self, prompt) -> str:
-        prompt = [{"role": "user", "content": prompt}]
+        prompt = f"Context: \n\nQuestion: {prompt}\n\nAnswer: "
 
         output = self.model(prompt)
-        output = output[0]
-        print(output)
-        output = output[0]["generated_text"].replace("<|end|>", "").strip(" ")
+        output = output[0]["generated_text"]
         return output
 
     def post_process(self, raw_text_output):
