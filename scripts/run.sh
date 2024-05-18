cb5e97b9253a7423d6704ff9da0b62b353fd375d
lekopien <mavericklekopien@gmail.com>
2024-05-19 01:47:31 +0300

    esolved issue of script not finding CSV data file

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+# Set the PYTHONPATH to include the project root directory
+export PYTHONPATH="/home/lekopien/development/new/AfriMed-QA:${PYTHONPATH}"
+
 pretrained_model_path="microsoft/Phi-3-mini-128k-instruct"
 # pretrained_model_path="gpt-4o"
 data_path="data/afri_med_qa_10k_v1_1_phase_1.csv"
 
 question_types=("mcq" "saq" "consumer_queries")
-prompt_type=base
+prompt_type="base"
 
 num_few_shot_values=(0)
 
@@ -13,7 +16,7 @@ for q_type in "${question_types[@]}"; do
     prompt_file_path="prompts/${prompt_type}_${q_type}.txt"
     echo $prompt_file_path
     for num_few_shot in "${num_few_shot_values[@]}"; do
-        python bin/main_predictions.py \
+        python3 ../bin/main_predictions.py \
             --data_path $data_path \
             --prompt_file_path $prompt_file_path \
             --pretrained_model_path $pretrained_model_path \
