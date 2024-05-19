c5ff1a2028d44d77da185308bf73d65b53eacc45
Aimerou <meroundiaye@gmail.com>
2024-05-19 14:07:48 +0000

    manage special tokens

--- src/models/biomistral.py
+++ src/models/biomistral.py
@@ -13,13 +13,15 @@ class BioMistral(Model):
         self.model = AutoModelForCausalLM.from_pretrained(pretrained_model_path, device_map="auto")
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+
     def predict(self, prompt) -> str:
         messages = [{"role": "user", "content": prompt}]
 
         model_inputs = self.tokenizer.apply_chat_template(messages, return_tensors="pt").to(self.device)
         generated_ids = self.model.generate(model_inputs, max_new_tokens=100, do_sample=True)
         output = self.tokenizer.batch_decode(generated_ids)[0]
-        output = output.replace("[INST] ", "").replace("[/INST] ", "").replace("<s>", "").replace("</s>", "").strip(" ").lstrip(" ")
+        output = output.replace("[INST] ", "").replace("[/INST] ", "").replace("<s>", "").replace("</s>", "").strip(" ")
+        if ouput[0]==" ": output = output[1:]
         print(output)
         return output
 
