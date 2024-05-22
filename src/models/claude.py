f7ffec16ac87a4132da9efe13c201f2026fcf9ef
SanniM3 <sannimardhiyah@gmail.com>
2024-05-22 18:17:35 +0100

    revert to default params

--- src/models/claude.py
+++ src/models/claude.py
@@ -31,8 +31,17 @@ class ClaudeModel(Model):
 
     def pattern_match(self, text, n=40):
         try:
-            return self.pattern1.match(text[:n]).groups()[2]
+            match = self.pattern1.match(text[:n])
+            if match is not None:
+                return match.groups()[2]
         except Exception:
-            return self.pattern2.match(text[:n]).groups()[2]
+            pass
+
+        try:
+            match = self.pattern2.match(text[:n])
+            if match is not None:
+                return match.groups()[2]
         except Exception:
-            return text[:n]
+            pass
+
+        return text[:n]
