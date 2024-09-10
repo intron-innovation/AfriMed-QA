41816b50f59322cf7f5eef009e6a1b4c31b4c6a1
owos <owodunniabraham@gmail.com>
2024-09-09 23:56:26 -0400

    adjust bash script

--- scripts/run_no_explanation.sh
+++ scripts/run_no_explanation.sh
@@ -9,7 +9,7 @@ declare -A model_paths
 model_paths["microsoft-phi-med128"]="microsoft/Phi-3-medium-128k-instruct"
 model_paths["microsoft-phi-mini128"]="microsoft/Phi-3-mini-128k-instruct"
 model_paths["meta-llama"]="meta-llama/Meta-Llama-3-8B"
-model_paths["meta-llama-405b"]="meta/llama3-405b-instruct-maas"
+# model_paths["meta-llama-405b"]="meta/llama3-405b-instruct-maas"
 
 source="afrimed-qa-v2"
 pretrained_model_choice="meta-llama-405b"
@@ -22,17 +22,27 @@ prompt_type="base"
 explanation=False
 num_few_shot_values=(0)
 
-for q_type in "${question_types[@]}"; do
-    prompt_file_path="prompts/${prompt_type}_mcq_no_exp.txt"
-    echo $prompt_file_path
-    for num_few_shot in "${num_few_shot_values[@]}"; do
-        python bin/main_predictions.py \
-            --data_path "$data_path" \
-            --prompt_file_path "$prompt_file_path" \
-            --explanation "$explanation" \
-            --pretrained_model_path "$pretrained_model_path" \
-            --source "$source" \
-            --q_type "$q_type" \
-            --num_few_shot "$num_few_shot"
+for model_key in "${!model_paths[@]}"; do
+    pretrained_model_path=${model_paths[$model_key]}
+    data_path=${data_paths[$source]}
+    
+    echo "Running model: $model_key"
+    echo "Using data: $data_path"
+    
+    # Loop through question types and few-shot values
+    for q_type in "${question_types[@]}"; do
+        prompt_file_path="prompts/${prompt_type}_mcq_no_exp.txt"
+        echo "Prompt file: $prompt_file_path"
+        
+        for num_few_shot in "${num_few_shot_values[@]}"; do
+            python bin/main_predictions.py \
+                --data_path "$data_path" \
+                --prompt_file_path "$prompt_file_path" \
+                --explanation "$explanation" \
+                --pretrained_model_path "$pretrained_model_path" \
+                --source "$source" \
+                --q_type "$q_type" \
+                --num_few_shot "$num_few_shot"
+        done
     done
 done
